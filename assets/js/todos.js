25e66223502fcfe6285d03cb6deac07fafa57596
yichenhao <yichen.hao@wustl.edu>
2018-12-13 16:21:23 -0600

    Final Version

---
+++ assets/js/todos.js
@@ -0,0 +1,28 @@
+//check off specific todos by clicking
+// $("li").click(function(){
+// 	$(this).toggleClass("completed");
+// });
+
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	//Enter key code is 13
+	if(event.which === 13){
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> "+todoText+"</li>");
+	}
+});
+
+$("#toggleBtn").click(function(){
+	$("input[type='text']").fadeToggle();
+});
